a729af19b9ac1ccad3df33044fda6599094194a4
koitt03-A <koitt03-A@koitt03-A-PC>
2018-09-21 09:37:01 +0900

    11

--- json/src/service/TicketService.java
+++ json/src/service/TicketService.java
@@ -13,11 +13,12 @@ public class TicketService {
 	
 	public Map<String,Object> selectTicketList(Ticket ticket){
 		Map<String,Object> rMap = new HashMap<String,Object>();
-		rMap.put("list", tdao.selectTicketList(ticket));
+		rMap.put("list", tdao.selectTicketList(ticket));				// 페이지 1~5 등 과 같이 페이지 클래스를 가져가기 위해 map 형태사용
 		rMap.put("page", 10);	//{ list: [{tm}]  ??
 		
 		return rMap;
 	
+		/*return tdao.selectTicketList(ticket);*/
 	}
 	
 	public Ticket selectTicket(Ticket ticket) {
